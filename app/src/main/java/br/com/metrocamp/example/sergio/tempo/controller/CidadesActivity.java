8a6551444c8f6b848d28ea5e1218319de773c6a0
Sergio Ordine <sjordine@yahoo.com.br>
2018-03-05 20:37:13 -0300

    Refactoring code

--- app/src/main/java/br/com/metrocamp/example/sergio/tempo/controller/CidadesActivity.java
+++ app/src/main/java/br/com/metrocamp/example/sergio/tempo/controller/CidadesActivity.java
@@ -1,7 +1,9 @@
-package br.com.metrocamp.example.sergio.tempo;
+package br.com.metrocamp.example.sergio.tempo.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.com.metrocamp.example.sergio.tempo.R;
 
 public class CidadesActivity extends AppCompatActivity {
 
