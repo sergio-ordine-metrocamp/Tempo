0a0419389907ac3328714d70431004bde9e26e97
Sergio Ordine <sjordine@yahoo.com.br>
2018-03-05 20:43:45 -0300

    Adicionando serviço e renomeando classes

---
+++ app/src/main/java/br/com/metrocamp/example/sergio/tempo/model/CidadeServices.java
@@ -0,0 +1,18 @@
+package br.com.metrocamp.example.sergio.tempo.model;
+
+import java.util.List;
+
+/**
+ * Created by Sergio on 3/5/18.
+ */
+
+public class CidadeServices {
+
+
+    public List<Clima> getClima() {
+
+        return null;
+
+    }
+
+}
--- app/src/main/java/br/com/metrocamp/example/sergio/tempo/model/Clima.java
+++ app/src/main/java/br/com/metrocamp/example/sergio/tempo/model/Clima.java
@@ -4,9 +4,36 @@ package br.com.metrocamp.example.sergio.tempo.model;
  * Created by Sergio on 3/5/18.
  */
 
-public enum Clima {
-    TEMPESTADE,
-    CHUVOSO,
-    NUBLADO,
-    SOL
+public class Clima {
+
+    //Attributes
+    private String nome;
+    private TipoClima tipoClima;
+    private int temperatura;
+
+    //Properties
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public TipoClima getTipoClima() {
+        return tipoClima;
+    }
+
+    public void setTipoClima(TipoClima tipoClima) {
+        this.tipoClima = tipoClima;
+    }
+
+
+    public int getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(int temperatura) {
+        this.temperatura = temperatura;
+    }
 }
