00f69aabcac0ee30c9d6e850d631f09c8031710a
Sergio Ordine <sjordine@yahoo.com.br>
2018-03-05 20:58:03 -0300

    Adicionando imagens

--- app/src/main/java/br/com/metrocamp/example/sergio/tempo/model/CidadeServices.java
+++ app/src/main/java/br/com/metrocamp/example/sergio/tempo/model/CidadeServices.java
@@ -1,5 +1,6 @@
 package br.com.metrocamp.example.sergio.tempo.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,9 +10,74 @@ import java.util.List;
 public class CidadeServices {
 
 
+
     public List<Clima> getClima() {
 
-        return null;
+        ArrayList<Clima> lista = new ArrayList<Clima>();
+
+        Clima clima = new Clima();
+
+        clima.setNome("Campinas");
+        clima.setTipoClima(TipoClima.NUBLADO);
+        clima.setTemperatura(32);
+        lista.add(clima);
+
+        clima.setNome("Americana");
+        clima.setTipoClima(TipoClima.SOL);
+        clima.setTemperatura(27);
+        lista.add(clima);
+
+        clima.setNome("São Paulo");
+        clima.setTipoClima(TipoClima.TEMPESTADE);
+        clima.setTemperatura(17);
+        lista.add(clima);
+
+        clima.setNome("Sousas");
+        clima.setTipoClima(TipoClima.NUBLADO);
+        clima.setTemperatura(30);
+        lista.add(clima);
+
+        clima.setNome("Hortolância");
+        clima.setTipoClima(TipoClima.CHUVOSO);
+        clima.setTemperatura(32);
+        lista.add(clima);
+
+        clima.setNome("Sorocaba");
+        clima.setTipoClima(TipoClima.TEMPESTADE);
+        clima.setTemperatura(25);
+        lista.add(clima);
+
+        clima.setNome("Santos");
+        clima.setTipoClima(TipoClima.SOL);
+        clima.setTemperatura(35);
+        lista.add(clima);
+
+        clima.setNome("Sumaré");
+        clima.setTipoClima(TipoClima.NUBLADO);
+        clima.setTemperatura(30);
+        lista.add(clima);
+
+        clima.setNome("Amparo");
+        clima.setTipoClima(TipoClima.CHUVOSO);
+        clima.setTemperatura(20);
+        lista.add(clima);
+
+        clima.setNome("Holambra");
+        clima.setTipoClima(TipoClima.SOL);
+        clima.setTemperatura(31);
+        lista.add(clima);
+
+        clima.setNome("Joaquim Egídio");
+        clima.setTipoClima(TipoClima.CHUVOSO);
+        clima.setTemperatura(29);
+        lista.add(clima);
+
+        clima.setNome("Limeira");
+        clima.setTipoClima(TipoClima.SOL);
+        clima.setTemperatura(27);
+        lista.add(clima);
+
+        return lista;
 
     }
 
